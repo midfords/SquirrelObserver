70441b6aedb35901000f612373832282dee746d0
Sean Midford <seanmidford@gmail.com>
2016-02-13 02:45:57 -0600

    Moved verify dialog to Utils class. Added physical back button listener.

--- app/src/main/java/com/project/squirrelobserver/squirrelObserver/RecordModifierTabActivity.java
+++ app/src/main/java/com/project/squirrelobserver/squirrelObserver/RecordModifierTabActivity.java
@@ -1,9 +1,13 @@
 package com.project.squirrelobserver.squirrelObserver;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.project.squirrelobserver.R;
+import com.project.squirrelobserver.util.Utils;
 
 /**
  * Created by sean on 2/9/16.
@@ -15,6 +19,16 @@ public  class RecordModifierTabActivity
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_modifiers_tab);
+    }
+
+    // This is a listener for the physical back button, so we can display the verification dialog here too
+    @Override
+    public void onBackPressed() {
+
+        Context context = RecordModifierTabActivity.this;
+
+        // Close current activity after verify dialogue
+        Utils.endRecordingSessionVerifyMessage(context, this);
     }
 
 }
